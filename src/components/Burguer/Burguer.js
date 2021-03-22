b86d4990705014b2a1d06b815adc36b8a4a957e5
Dijango Alves <dijango_ilha@hotmail.com>
2021-03-22 09:15:02 -0300

    feat: output burguer ingredients dynamic

--- src/components/Burguer/Burguer.js
+++ src/components/Burguer/Burguer.js
@@ -4,11 +4,15 @@ import BurguerIngredient from './BurguerIngredient/BurguerIngredient'
 import classes from './Burguer.css'
 
 const Burguer = props => {
+  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, index) => {
+      return <BurguerIngredient key={igKey + index} type={igKey} />
+    })
+  })
   return (
     <div className={classes.Burguer}>
       <BurguerIngredient type='bread-top' />
-      <BurguerIngredient type='cheese' />
-      <BurguerIngredient type='meat' />
+      {transformedIngredients}
       <BurguerIngredient type='bread-bottom' />
     </div>
   )
