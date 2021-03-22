d4329bf13e6f5f34e49f35f3692aaa311847e48c
Dijango Alves <dijango_ilha@hotmail.com>
2021-03-22 14:54:29 -0300

    feat: connect state to build controls

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -1,32 +1,50 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
 import Aux from '../../hoc/Aux'
 import Burguer from '../../components/Burguer/Burguer'
 import BuildControls from '../../components/Burguer/BuildControls/BuildControls'
 
-class BurguerBuilder extends Component {
-  // constructor (props) {
-  //   super(props)
-  //   this.state = {...}
-  // }
+const INGREDIENTS_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
 
-  state = {
-    ingredients: {
-      salad: 0,
-      bacon: 0,
-      cheese: 0,
-      meat: 0
+const BurguerBuilder = () => {
+  const [ingredients, setIngredients] = useState({
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0
+  })
+  const [totalPrice, setTotalPrice] = useState({ totalPrice: 4 })
+
+  const addIngredientHandler = type => {
+    const oldCount = ingredients[type]
+    const updateCount = oldCount + 1
+    const updatedIngredients = {
+      ...ingredients
     }
+
+    updatedIngredients[type] = updateCount
+
+    const priceAddition = INGREDIENTS_PRICES[type]
+    const oldPrice = totalPrice
+    const newPrice = oldPrice + priceAddition
+
+    setIngredients(updatedIngredients)
+    setTotalPrice(newPrice)
   }
 
-  render () {
-    return (
-      <Aux>
-        <Burguer ingredients={this.state.ingredients} />
-        <BuildControls />
-      </Aux>
-    )
-  }
+  const removeIngredientHandler = type => {}
+
+  return (
+    <Aux>
+      <Burguer ingredients={ingredients} />
+      <BuildControls ingredientAdded={addIngredientHandler} />
+    </Aux>
+  )
 }
 
 export default BurguerBuilder
