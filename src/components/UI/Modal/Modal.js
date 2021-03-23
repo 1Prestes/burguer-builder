ab560acaf99a6f3aa6d0d629eba220fa638a9c96
Dijango Alves <dijango_ilha@hotmail.com>
2021-03-23 12:49:05 -0300

    refactor: improve performance

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Aux/Aux'
 import Backdrop from '../Backdrop/Backdrop'
 
 import classes from './Modal.css'
 
-const Modal = props => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-      className={classes.Modal}
-    >
-      {props.children}
-    </div>
-  </Aux>
-)
-
-export default Modal
+const Modal = props => {
+  console.log('aaa')
+  return (
+    <Aux>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        style={{
+          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: props.show ? '1' : '0'
+        }}
+        className={classes.Modal}
+      >
+        {props.children}
+      </div>
+    </Aux>
+  )
+}
+const showIsEqual = (prevProps, nextProps) => {
+  return prevProps.show === nextProps.show
+}
+export default React.memo(Modal, showIsEqual)
