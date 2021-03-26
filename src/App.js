4608a6a92689cfc8730fdeda725725a63394b0fe
Dijango Alves <dijango_ilha@hotmail.com>
2021-03-26 20:45:27 -0300

    feat: add an orders page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { Route, Switch } from 'react-router-dom'
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
+import Orders from './containers/Orders/Orders'
 
-const App = props => {
+const App = () => {
   return (
     <div>
       <Layout>
         <Switch>
           <Route path='/checkout' component={Checkout} />
+          <Route path='/orders' component={Orders} />
           <Route path='/' component={BurgerBuilder} />
         </Switch>
       </Layout>
