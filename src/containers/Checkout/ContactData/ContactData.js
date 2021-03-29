1fc9003835e4dc8cae439ec3cb26bc896dc39f43
Dijango Alves <dijango_ilha@hotmail.com>
2021-03-29 09:15:15 -0300

    feat: create a custom dynamic input component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import axios from '../../../axios-orders'
 
 import Button from '../../../components/UI/Button/Button'
+import Input from '../../../components/UI/Input/Input'
 import Spinner from '../../../components/UI/Spinner/Spinner'
 import classes from './ContactData.css'
 
@@ -47,26 +48,21 @@ const ContactData = props => {
 
   let form = (
     <form>
-      <input
-        className={classes.Input}
+      <Input
+        inputtype='input'
         type='text'
         name='name'
         placeholder='Your Name'
       />
-      <input
-        className={classes.Input}
+      <Input
+        inputtype='input'
         type='email'
         name='email'
         placeholder='Your E-mail'
       />
-      <input
-        className={classes.Input}
-        type='text'
-        name='street'
-        placeholder='Street'
-      />
-      <input
-        className={classes.Input}
+      <Input inputtype='input' type='text' name='street' placeholder='Street' />
+      <Input
+        inputtype='input'
         type='text'
         name='postal'
         placeholder='Postal Code'
