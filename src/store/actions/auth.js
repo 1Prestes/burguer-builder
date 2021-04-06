0c7914f3689fc57d6d3eea1e1d589c3ccdd6b8e7
Dijango Alves <dijango_ilha@hotmail.com>
2021-04-06 20:19:02 -0300

    feat: logging users out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -16,12 +16,27 @@ export const authSuccess = (token, userId) => {
   }
 }
 
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  }
+}
+
 export const authFail = error => {
   return {
     type: actionTypes.AUTH_FAIL,
     error
   }
 }
+
+export const checkAuthTimeout = expirationOnTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout())
+    }, expirationOnTime * 1000)
+  }
+}
+
 export const auth = (email, password, isSignUp) => {
   return dispatch => {
     dispatch(authStart())
@@ -41,6 +56,7 @@ export const auth = (email, password, isSignUp) => {
       .then(response => {
         console.log(response)
         dispatch(authSuccess(response.data.idToken, response.data.localId))
+        dispatch(checkAuthTimeout(response.data.expiresIn))
       })
       .catch(error => {
         console.log(error)
