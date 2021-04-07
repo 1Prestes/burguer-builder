a67688eb9a43cf2761d284edced87ff62ad43eea
Dijango Alves <dijango_ilha@hotmail.com>
2021-04-07 20:43:02 -0300

    test: test containers

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import withErrorHandler from '../../withErrorHandler/withErrorHandler'
 import * as actions from '../../store/actions/'
 import axios from '../../axios-orders'
 
-const BurgerBuilder = props => {
+export const BurgerBuilder = props => {
   const [purchasing, setPurchasing] = useState(false)
 
   useEffect(() => {
